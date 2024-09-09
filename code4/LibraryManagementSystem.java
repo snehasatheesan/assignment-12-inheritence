3f046e3491c895a1c980d01da563bb60d99eb4e5
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-09 12:49:32 +0530

    Create LibraryManagementSystem.java

---
+++ code4/LibraryManagementSystem.java
@@ -0,0 +1,16 @@
+package inheritence;
+
+public class LibraryManagementSystem {
+    String userType;
+    String userName;
+    String password;
+    public void login(){
+
+    }
+    public void register(){
+
+    }
+    public void logout(){
+
+    }
+}
