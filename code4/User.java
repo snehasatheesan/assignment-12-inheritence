62fccc1431be23fdc3327fcdef5853c5f9171c28
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-09 12:46:48 +0530

    Create User.java

---
+++ code4/User.java
@@ -0,0 +1,18 @@
+package inheritence;
+
+public class User {
+    String name;
+    String id;
+
+    public void verify() {
+
+    }
+
+    public void checkAccount() {
+
+    }
+
+    public void getBookInfo() {
+
+    }
+}
