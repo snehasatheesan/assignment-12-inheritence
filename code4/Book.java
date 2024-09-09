55e5b65aaa6f685796d720d52dca483d62d84d7a
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-09 12:51:07 +0530

    Create Book.java

---
+++ code4/Book.java
@@ -0,0 +1,28 @@
+package inheritence;
+
+public class Book {
+    String title;
+    String author;
+    String isbn;
+    String publication;
+
+    public void showDue() {
+
+    }
+
+    public void reservationStatus() {
+
+    }
+
+    public void feedback() {
+
+    }
+
+    public void bookRequest() {
+
+    }
+
+    public void renewInfo() {
+
+    }
+}
