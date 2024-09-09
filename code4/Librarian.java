f9d83232820acb51e28ebcc008eb2738101020c9
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-09 12:50:37 +0530

    Create Librarian.java

---
+++ code4/Librarian.java
@@ -0,0 +1,15 @@
+package inheritence;
+public class Librarian {
+    String name;
+    String id;
+    String password;
+    String searchString;
+
+    public void verifyLibrarian() {
+
+    }
+
+    public void search() {
+
+    }
+}
