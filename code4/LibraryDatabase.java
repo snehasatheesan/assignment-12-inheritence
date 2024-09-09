24bf3d4fce054e0257ef6cc9a1ad6b637474f543
snehasatheesan <snehasatheesan13@gmail.com>
2024-09-09 12:50:00 +0530

    Create LibraryDatabase.java

---
+++ code4/LibraryDatabase.java
@@ -0,0 +1,27 @@
+package inheritence;
+
+import java.util.List;
+
+public class LibraryDatabase {
+    List<Book> listOfBooks;
+
+    public void add() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void display() {
+
+    }
+
+    public void search() {
+
+    }
+}
